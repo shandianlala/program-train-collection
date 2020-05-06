cbfe8968f67d71090fa32302c3055db6e51fae51
shandianlala <shandianlala@gmai.com>
2020-05-06 23:04:47 +0800

    修复公平锁方式，数组越出异常

--- juc/src/test/ThreadSchedulingTest.java
+++ juc/src/test/ThreadSchedulingTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -13,25 +14,26 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ThreadSchedulingTest {
 
-    public volatile int readIndex = 0;
+    public volatile AtomicInteger readIndex = new AtomicInteger(0);
 
     public List<String> stringList = Arrays.asList("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
 
     public ReentrantLock lock = new ReentrantLock(true);
 
+    /**
+     * 使用对象监视器的方式实现（object monitor）
+     */
     @Test
     public void testThreadScheduling() {
         Thread thread1 = generateThread("Thread-0");
         Thread thread2 = generateThread("Thread--1");
         thread1.start();
         thread2.start();
-
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.out.println("thread1 in main method, state=" + thread1.getState().name());
         System.out.println("thread2 in main method, state=" + thread2.getState().name());
     }
@@ -40,9 +42,9 @@ public class ThreadSchedulingTest {
         Thread thread = new Thread(() -> {
             synchronized (stringList) {
                 String name = Thread.currentThread().getName();
-                while (readIndex < stringList.size()) {
-                    System.out.println(name + " | " + stringList.get(readIndex));
-                    readIndex++;
+                while (readIndex.get() < stringList.size()) {
+                    System.out.println(name + " | " + stringList.get(readIndex.get()));
+                    readIndex.getAndIncrement();
                     try {
                         stringList.notify();
                         stringList.wait();
@@ -56,10 +58,13 @@ public class ThreadSchedulingTest {
         return thread;
     }
 
+    /**
+     * 使用reentrantLock 公平锁的方式实现
+     */
     @Test
     public void testThreadScheduling1() {
         Thread thread1 = generateThread1("Thread-0");
-        Thread thread2 = generateThread1("Thread-1");
+        Thread thread2 = generateThread1("Thread - 1");
         thread2.start();
         thread1.start();
 
@@ -77,10 +82,12 @@ public class ThreadSchedulingTest {
         Thread thread = new Thread(() -> {
             String name = Thread.currentThread().getName();
             System.out.println(name + " in run method, state=" + Thread.currentThread().getState().name());
-            while (readIndex < stringList.size()) {
+            int index;
+            while ((index = readIndex.get()) < stringList.size()) {
                 lock.lock();
-                System.out.println(name + " | " + stringList.get(readIndex));
-                readIndex++;
+                if (readIndex.compareAndSet(index, index + 1)) {
+                    System.out.println(name + " | " + stringList.get(index));
+                }
                 lock.unlock();
             }
         });
