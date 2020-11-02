6cc45617f97b97bde04ce5c97c47ad61d844de49
shandianlala <shandianlala@gmai.com>
2020-11-03 01:35:17 +0800

    feat：add CyclicBarrierTest

---
+++ juc/src/main/java/club/sdll/ptc/juc/cyclicbarrier/CyclicBarrierTest.java
@@ -0,0 +1,66 @@
+package club.sdll.ptc.juc.cyclicbarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * description
+ *
+ * @author shandianlala@gmail.com
+ * @version 1.0
+ * @date 2020-11-01 22:50
+ */
+public class CyclicBarrierTest {
+
+    static CyclicBarrier c = new CyclicBarrier(2, new A());
+
+    static CyclicBarrier c2 = new CyclicBarrier(2);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1; i++) {
+            testCommand();
+            System.out.println("=========");
+        }
+
+    }
+
+    public static void testCommand() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+//                    Thread.sleep(1500L);
+                    c.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(1 + ", thread is " + Thread.currentThread().getName());
+            }
+        }).start();
+
+        try {
+            Thread.sleep(1L);
+            c.await();
+//            Thread.sleep(1L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(2 + ", thread is " + Thread.currentThread().getName());
+    }
+
+    static class A implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(3 + ", thread is " + Thread.currentThread().getName());
+        }
+    }
+
+
+
+
+}
